718d1bb55dee319b3111a3764723628cb9f21712
gabriel7augustobc <gabriel_abcunha@hotmail.com>
2018-04-23 22:23:18 -0300

    Tela de temas

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
   $border: '#979797',
@@ -12,5 +15,5 @@ EStyleSheet.build({
   $darkText: '#343434',
 });
 
-export default () => <Options />;
+export default () => <Themes />;
 
