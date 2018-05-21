c0447367eaa5337a4109b1be27cfc1c2704ba584
Gabriel Augusto <gabriel_abcunha@hotmail.com>
2018-05-21 15:30:37 -0300

    Até exibindo mensagens

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Navigator from './config/routes';
@@ -16,3 +16,28 @@ EStyleSheet.build({
 });
 
 export default () => <Navigator />;
+*/
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Navigator from './config/routes';
+import { AlertProvider } from './components/Alert';
+
+EStyleSheet.build({
+  $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+  $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
+  $darkText: '#343434',
+});
+
+export default () => (
+  <AlertProvider>
+    <Navigator />
+  </AlertProvider>
+);
+
--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+/*import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
@@ -44,3 +44,58 @@ class Options extends Component {
   }
 }
 export default Options;
+
+*/
+
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+
+class Options extends Component {
+  static propTypes = {
+    navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
+  };
+
+  handlePressThemes = () => {
+    this.props.navigation.navigate('Themes');
+  };
+
+  handlePressSite = () => {
+    Linking.openURL('telnet://fixer.io/').catch(() =>
+      this.props.alertWithType('error', 'Sorry!', "Fixer.io can't be opened right now."));
+    // info, warn, error, success, custom
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default" />
+        <ListItem
+          text="Themes"
+          onPress={this.handlePressThemes}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} size={ICON_SIZE} color={ICON_COLOR} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handlePressSite}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} size={ICON_SIZE} color={ICON_COLOR} />}
+        />
+        <Separator />
+      </ScrollView>
+    );
+  }
+}
+export default connectAlert(Options);
+
