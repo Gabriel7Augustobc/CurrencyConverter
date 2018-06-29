17c93efef9ca809dd5e4712c28973f0eb4f77733
Gabriel Augusto <gabriel_abcunha@hotmail.com>
2018-06-29 17:06:04 -0300

    Até-Introdução Gerenciando estado da aplicação

--- app/index.js
+++ app/index.js
@@ -1,27 +1,12 @@
-/*import React from 'react';
-import EStyleSheet from 'react-native-extended-stylesheet';
-
-import Navigator from './config/routes';
-
-EStyleSheet.build({
-  $primaryBlue: '#4F6D7A',
-  $primaryOrange: '#D57A66',
-  $primaryGreen: '#00BD9D',
-  $primaryPurple: '#9E768F',
-  $white: '#FFFFFF',
-  $lightGray: '#F0F0F0',
-  $border: '#979797',
-  $inputText: '#797979',
-  $darkText: '#343434',
-});
-
-export default () => <Navigator />;
-*/
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Provider } from 'react-redux';
 
 import Navigator from './config/routes';
+import store from './config/store';
 import { AlertProvider } from './components/Alert';
+
+import './reducers';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -36,8 +21,9 @@ EStyleSheet.build({
 });
 
 export default () => (
-  <AlertProvider>
-    <Navigator />
-  </AlertProvider>
+  <Provider store={store}>
+    <AlertProvider>
+      <Navigator onNavigationStateChange={null} />
+    </AlertProvider>
+  </Provider>
 );
-
